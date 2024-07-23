d4cc33ce5253badfebb7cc0059b08e9cfdb418f4
letsmovetocloud <letsmovetocloud@gmail.com>
2024-07-23 18:34:34 +0530

    kiran:vmdeployment code

---
+++ vmlab.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "kiranresourcgroup"
+  location = "us-east-region"
+}
+
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  is_windows_image    = true
+  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+  admin_password      = "EASTEASTgl0ba1!"
+  vm_os_simple        = "WindowsServer"
+  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.example]
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.example]
+}
+
+
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
+}
